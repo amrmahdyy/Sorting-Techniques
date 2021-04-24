7c266233f0f1949f8905dc1f3a2cd1116fdc32b3
amrmahdyy <amrmahdy2@gmail.com>
2021-04-24 17:24:19 +0200

    added InsertionSort class with its function test cases are neeeded

---
+++ src/InsertionSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    static void swap(int[]arr,int i,int left){
+        int temp=arr[i];
+        arr[i]=arr[left];
+        arr[left]=temp;
+    }
+    static void sort(int[]arr){
+
+        for(int i=1;i<arr.length;i++){
+            int left=i-1;
+            int current=i;
+            while(left>=0 && arr[current]<arr[left]){
+                swap(arr,current,left);
+                current--;
+                left--;
+            }
+        }
+    }
+    public static void main(String[]args){
+        int[]arr={-50,20,-89,2124,-5392};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
