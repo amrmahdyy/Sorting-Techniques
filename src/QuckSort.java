89e7b30850339a6a4a330986a42354df631b4cf2
amrmahdyy <amrmahdy2@gmail.com>
2021-05-03 21:30:48 +0200

    added quick sort method

--- src/QuckSort.java
+++ src/QuckSort.java
@@ -8,7 +8,7 @@ public class QuckSort {
     }
     public static int partition(int[]arr,int start,int end){
         int pivot=arr[end];
-        int startPivot=0;
+        int startPivot=start;
         for(int i=start;i<end;i++){
             if(arr[i]<=pivot){
                swap(arr,i,startPivot);
@@ -18,9 +18,16 @@ public class QuckSort {
         swap(arr,startPivot,end);
        return startPivot;
     }
-
+    public static void quickSort(int[]arr,int start,int end){
+        if(start<end) {
+            int pivotIndex = partition(arr, start, end);
+            quickSort(arr, start, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, end);
+        }
+    }
     public static void main(String[]args){
-        int[]arr={7,2,1,6,8,5,3,4};
-        System.out.println(arr[partition(arr,0,arr.length-1)]);
+        int[]arr={7,2,1,6,5};
+        quickSort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
     }
 }
